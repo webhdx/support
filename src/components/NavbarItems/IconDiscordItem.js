3532bd0b3f2e76b91ac2948a10b25051822a5f22
Maciej Kobus <webhdx@gmail.com>
2024-08-10 16:03:31 +0200

    Make Discord and GitHub navbar links to open in new tab

--- src/components/NavbarItems/IconDiscordItem.js
+++ src/components/NavbarItems/IconDiscordItem.js
@@ -3,7 +3,7 @@ import { IconBrandDiscord } from '@tabler/icons-react';
 export default function IconDiscordItem() {
     return (
         <div class="header-link">
-            <a href="https://click.webhdx.dev/discord" class="clean-btn header-link-button">
+            <a href="https://click.webhdx.dev/discord" target="_blank" class="clean-btn header-link-button">
                 <IconBrandDiscord stroke={2} />
             </a>
         </div>
--- src/components/NavbarItems/IconGitHubItem.js
+++ src/components/NavbarItems/IconGitHubItem.js
@@ -3,7 +3,7 @@ import { IconBrandGithub } from '@tabler/icons-react';
 export default function IconGitHubItem() {
     return (
         <div class="header-link">
-            <a href="https://github.com/webhdx/support" class="clean-btn header-link-button">
+            <a href="https://github.com/webhdx/support" target="_blank" class="clean-btn header-link-button">
                 <IconBrandGithub stroke={2} />
             </a>
         </div>
