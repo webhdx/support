c989667cea6f954aa993915807fbb127608250d3
Maciej Kobus <webhdx@gmail.com>
2025-05-19 21:07:57 +0200

    Refactor NavbarItems to use className instead of class for JSX compatibility

--- src/components/NavbarItems/IconDiscordItem.js
+++ src/components/NavbarItems/IconDiscordItem.js
@@ -2,8 +2,8 @@ import { IconBrandDiscord } from '@tabler/icons-react';
 
 export default function IconDiscordItem() {
     return (
-        <div class="header-link">
-            <a href="https://click.webhdx.dev/discord" target="_blank" class="clean-btn header-link-button">
+        <div className="header-link">
+            <a href="https://click.webhdx.dev/discord" target="_blank" className="clean-btn header-link-button">
                 <IconBrandDiscord stroke={2} />
             </a>
         </div>
